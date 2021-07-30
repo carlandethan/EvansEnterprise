a8a7a35416d8002d6dae2dd8788e8b1416c624ee
Carl Evans <carlandethan@gmail.com>
2021-07-29 20:45:30 -0500

    First commit for EvansEnterprise

---
+++ EvansEnterprise/Data/ToDoItemContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EvansEnterprise.Model;
+
+namespace EvansEnterprise.Data
+{
+    public class ToDoItemContext : DbContext
+    {
+        public ToDoItemContext (DbContextOptions<ToDoItemContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EvansEnterprise.Model.ToDoItem> ToDoItem { get; set; }
+    }
+}
